749ce064f8bf884a3ba5a6dd3baf00380871760c
alan.zambrano <alan.zambrano@globant.com>
2017-01-24 17:04:16 -0600

    Moved API constants to RiotApiConstants

--- src/services/apiBuilder.js
+++ src/services/apiBuilder.js
@@ -1,12 +1,11 @@
-const RIOT_LAN_ENDPOINT = "https://lan.api.pvp.net";
-const API_KEY = "21682440-4d87-4971-bc31-478519c61a20";
+import RiotApiConstants from '../constants/riotApiConstants';
 
 function getSummonerDataUrl(summonerName) {
     return buildApiUrl(`/api/lol/LAN/v1.4/summoner/by-name/${summonerName}`)
 }
 
 function buildApiUrl(apiUrl) {
-    return `${RIOT_LAN_ENDPOINT}/${apiUrl}?api_key=${API_KEY}`;
+    return `${RiotApiConstants.RIOT_LAN_ENDPOINT}/${apiUrl}?api_key=${RiotApiConstants.API_KEY}`;
 }
 
 module.exports = {
