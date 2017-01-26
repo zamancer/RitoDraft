53144d0252858c912184169e83242b2450866eed
alan.zambrano <alan.zambrano@globant.com>
2017-01-26 12:33:58 -0600

    Added Username box for requesting info for different players

--- src/components/reloader/reloader.js
+++ src/components/reloader/reloader.js
@@ -1,32 +1,139 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+//@Components
 import Button from 'react-bootstrap/lib/Button';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import ControlLabel from 'react-bootstrap/lib/ControlLabel';
+import FormControl from 'react-bootstrap/lib/FormControl';
+import Form from 'react-bootstrap/lib/Form';
+//@Services
+import ApiBuilder from '../../services/apiBuilder';
+import RiotApi from '../../services/riotApi';
+//@Libs
+import Axios from 'axios';
+import _find from 'lodash/find';
 
 class Reloader extends Component {
     constructor() {
         super();
-
         this.state = {
-            isButtonEnabled: true
+            isButtonEnabled: true,
+            inputValue: 'Zamancer',
         };
+        
+        this.updateInputValue = this.updateInputValue.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.hideModal = this.hideModal.bind(this);
     }
 
-    getReloadButton = function() {
+    handleClick() {
+        
+        this.props.showModal(true);
 
-        return <Button  bsSize={"large"} 
-                        bsStyle={"primary"} 
-                        disabled={!this.state.isButtonEnabled} 
-                        onClick={this.props.handleReload}>
-                    Reload 
-                </Button>;
+        let summonerName = this.state.inputValue;
+        
+        this.getUserInfo(summonerName.toLowerCase())
+            .then(this.getGameInfo)
+            .then(this.getChampionInfo)
+            .then(this.props.handleReload)
+            .catch(this.hideModal);
+    }
+
+    hideModal(){
+        this.props.showModal(false);
+    }
+
+    getUserInfo = function(username) {
+
+        const apiUrl = ApiBuilder.getSummonerDataUrl(username);
+        
+        return RiotApi.get(apiUrl, {}, 
+                function (response) {
+                
+                    const userRemoteData = response.data[username];
+                    
+                    const userData = {userId: userRemoteData.id,
+                                    username: userRemoteData.name, 
+                                    level: userRemoteData.summonerLevel, 
+                                    revisionDate: new Date(userRemoteData.revisionDate).toDateString()};
+
+                    return userData;
+                }, 
+        () => alert('Username not found'));
+    }
+
+    getGameInfo(userInfo) {      
+        
+        const apiUrl = ApiBuilder.getRecentGamesUrl(userInfo.userId);
+
+        return RiotApi.get(apiUrl, {}, function(response){
+ 
+            const games = response.data.games;
+
+            let gameCards = games.map((game) => {
+                const localGameDate = new Date(game.createDate);
+
+                return {
+                    result: game.stats.win === true ? "Win" : "Defeat",
+                    timeSpent: "27m 22s",
+                    gameDate: `${localGameDate.toLocaleDateString()} ${localGameDate.toLocaleTimeString()}`,
+                    kda: (game.stats.championsKilled + game.stats.assists) / game.stats.numDeaths,
+                    cs: game.stats.minionsKilled + game.stats.neutralMinionsKilled,
+                    gold: game.stats.goldEarned,
+                    champId: game.championId
+                };
+            });
+
+            return { userData: userInfo, gameCards};
+        });
+    }
+
+    getChampionInfo(gameInfo){
+
+        const gameCards = gameInfo.gameCards;
+
+        let axiosRequests = gameCards.map((gameCard) => {
+            const apiUrl = ApiBuilder.getChampionDataUrl(gameCard.champId);
+            return Axios.get(apiUrl);
+        });
+
+        return Axios.all(axiosRequests)
+            .then(function (obj) {
+                
+                let gameCardsWithChamp = obj.map((response) => {
+                    
+                    let foundChamp = {champ: response.data.key};
+                    let gameCard = _find(gameCards, { champId: response.data.id });
+                    
+                    return Object.assign({}, gameCard, foundChamp);
+                });
+
+                return Object.assign({}, gameInfo, {gameCards: gameCardsWithChamp});
+            
+            })
+            .catch(function(error){ console.log(error) });
+    }
+
+    updateInputValue(evt) {
+        this.setState({
+            inputValue: evt.target.value
+        });
     }
 
     render() {
-        const reloadButton = this.getReloadButton();
-
         return (
             <div className="component-block">
-                {reloadButton}
+                <Form inline>
+                    <FormGroup controlId="formInlineName">
+                    <ControlLabel>Username</ControlLabel>
+                    {'   '}
+                    <FormControl type="text" value={this.state.inputValue} placeholder="Zamancer" onChange={this.updateInputValue} />
+                    </FormGroup>
+                    {'   '}
+                    <Button bsStyle={"primary"} onClick={this.handleClick} >
+                        Load
+                    </Button>
+                </Form>
             </div>
         );
     }
--- src/routes/dashboardView/dashboardView.js
+++ src/routes/dashboardView/dashboardView.js
@@ -4,13 +4,8 @@ import UserInfo from '../../components/user-info/user-info';
 import Reloader from '../../components/reloader/reloader';
 import GameCard from '../../components/gamecard/gamecard';
 import Modal from 'react-bootstrap/lib/Modal';
-//@Services
-import ApiBuilder from '../../services/apiBuilder';
-import RiotApi from '../../services/riotApi';
 //@Libs
 import 'bootstrap/dist/css/bootstrap.css';
-import Axios from 'axios';
-import _find from 'lodash/find';
 
 class DasboardView extends Component {
     constructor() {
@@ -21,108 +16,26 @@ class DasboardView extends Component {
             gameCards: []
         }
 
-        this.handleClick = this.handleClick.bind(this);
+        this.isModalOpen = this.isModalOpen.bind(this);
         this.updateStateFromReload = this.updateStateFromReload.bind(this);
     }
 
-    componentDidMount(){
-        this.getUserInfo('zamancer');
-    }
-
-    getUserInfo = function(username) {
-
-        const dashboard = this;
-        
-        const apiUrl = ApiBuilder.getSummonerDataUrl(username);
-        
-        RiotApi.get(apiUrl, {}, 
-                function (response) {
-                
-                    const userRemoteData = response.data[username];
-                    
-                    const userData = {userId: userRemoteData.id,
-                                    username: userRemoteData.name, 
-                                    level: userRemoteData.summonerLevel, 
-                                    revisionDate: new Date(userRemoteData.revisionDate).toDateString()};
-
-                    dashboard.setState({
-                        userData
-                    });
-                }, 
-        () => alert('Username not found'));
-    }
-
-    updateStateFromReload(gameCards) {
-        
+    isModalOpen(value) {
         this.setState({
-                    gameCards,
+            showModal: value
+        });
+    }
+
+    updateStateFromReload(fetchedData) {
+    
+        this.setState({
+                    userData: fetchedData.userData,
+                    gameCards: fetchedData.gameCards,
                     showModal: false
                 });
     }
 
-    handleClick() {
-        
-        this.setState({
-            showModal: true
-        });
-
-        this.getGameInfo()
-            .then(this.getChampionInfo)
-            .then(this.updateStateFromReload)
-            .catch();
-    }
-
-    getGameInfo() {      
-        
-        const apiUrl = ApiBuilder.getRecentGamesUrl(this.state.userData.userId);
-
-        return RiotApi.get(apiUrl, {}, function(response){
- 
-            const games = response.data.games;
-
-            let gameCards = games.map((game) => {
-                const localGameDate = new Date(game.createDate);
-
-                return {
-                    result: game.stats.win === true ? "Win" : "Defeat",
-                    timeSpent: "27m 22s",
-                    gameDate: `${localGameDate.toLocaleDateString()} ${localGameDate.toLocaleTimeString()}`,
-                    kda: (game.stats.championsKilled + game.stats.assists) / game.stats.numDeaths,
-                    cs: game.stats.minionsKilled + game.stats.neutralMinionsKilled,
-                    gold: game.stats.goldEarned,
-                    champId: game.championId
-                };
-            });
-
-            return gameCards;
-        });
-    }
-
-    getChampionInfo(gameCards){
-
-        let axiosRequests = gameCards.map((gameCard) => {
-            const apiUrl = ApiBuilder.getChampionDataUrl(gameCard.champId);
-            return Axios.get(apiUrl);
-        });
-
-        return Axios.all(axiosRequests)
-            .then(function (obj) {
-                
-                let gameCardsWithChamp = obj.map((response) => {
-                    
-                    let foundChamp = {champ: response.data.key};
-                    let gameCard = _find(gameCards, { champId: response.data.id });
-                    
-                    return Object.assign({}, gameCard, foundChamp);
-                });
-
-                return gameCardsWithChamp;
-            
-            })
-            .catch(function(error){ console.log(error) });
-    }
-
-    _getGameCards(){
+    _getGameCards() {
         return this.state.gameCards.map((gameCard, i) => {
             return <GameCard {...gameCard} key={i} />
         });
@@ -133,7 +46,7 @@ class DasboardView extends Component {
 
         return (
                 <div>
-                    <Reloader handleReload={this.handleClick} />
+                    <Reloader userId={this.state.userData.userId} showModal={this.isModalOpen} handleReload={this.updateStateFromReload} />
                     <UserInfo {...this.state.userData} />
                     {gameCards}
                     <Modal show={this.state.showModal} > 
